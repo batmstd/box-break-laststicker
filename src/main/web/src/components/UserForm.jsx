c1bd5a321e444a3cf313003aa957dc5c6a8cb17f
oda <dmitriy.oschepkov@sl-e.ru>
2021-07-16 18:46:02 +0300

    commands logo

--- src/main/web/src/components/UserForm.jsx
+++ src/main/web/src/components/UserForm.jsx
@@ -3,6 +3,7 @@ import {activeDrafts, addUserToDraft, draft} from "../api";
 import {SortableContainer, SortableElement} from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 import {NavLink} from "react-router-dom";
+import * as NBAIcons from 'react-nba-logos';
 
 export const UserForm = () => {
     const [breaks, setBreaks] = React.useState([]);
@@ -66,8 +67,78 @@ export const UserForm = () => {
 
 const SortableItem = SortableElement(({value, i}) => <div className={"wrapped-team"}>
     <div>{i}.</div>
-    <div className={"team"}>{value}</div>
+    <div className={"team"}>
+        <Logo team={value}/>
+        <div>{value}</div>
+    </div>
 </div>);
+
+const Logo = ({team}) => {
+    switch (team) {
+        case "Atlanta":
+            return <NBAIcons.ATL size={40}/>
+        case "Boston":
+            return <NBAIcons.BOS size={40}/>
+        case "Brooklyn":
+            return <NBAIcons.BKN size={40}/>
+        case "Charlotte":
+            return <NBAIcons.CHA size={40}/>
+        case "Chicago":
+            return <NBAIcons.CHI size={40}/>
+        case "Cleveland":
+            return <NBAIcons.CLE size={40}/>
+        case "Dallas":
+            return <NBAIcons.DAL size={40}/>
+        case "Denver":
+            return <NBAIcons.DEN size={40}/>
+        case "Detroit":
+            return <NBAIcons.DET size={40}/>
+        case "Golden State":
+            return <NBAIcons.GSW size={40}/>
+        case "Houston":
+            return <NBAIcons.HOU size={40}/>
+        case "Indiana":
+            return <NBAIcons.IND size={40}/>
+        case "LA Clippers":
+            return <NBAIcons.LAC size={40}/>
+        case "LA Lakers":
+            return <NBAIcons.LAL size={40}/>
+        case "Memphis":
+            return <NBAIcons.MEM size={40}/>
+        case "Miami":
+            return <NBAIcons.MIA size={40}/>
+        case "Milwaukee":
+            return <NBAIcons.MIL size={40}/>
+        case "Minnesota":
+            return <NBAIcons.MIN size={40}/>
+        case "New Orleans":
+            return <NBAIcons.NOP size={40}/>
+        case "New York":
+            return <NBAIcons.NYK size={40}/>
+        case "Oklahoma City":
+            return <NBAIcons.OKC size={40}/>
+        case "Orlando":
+            return <NBAIcons.ORL size={40}/>
+        case "Philadelphia":
+            return <NBAIcons.PHI size={40}/>
+        case "Phoenix":
+            return <NBAIcons.PHX size={40}/>
+        case "Portland":
+            return <NBAIcons.POR size={40}/>
+        case "Sacramento":
+            return <NBAIcons.SAC size={40}/>
+        case "San Antonio":
+            return <NBAIcons.SAS size={40}/>
+        case "Toronto":
+            return <NBAIcons.TOR size={40}/>
+        case "Utah":
+            return <NBAIcons.UTA size={40}/>
+        case "Washington":
+            return <NBAIcons.WAS size={40}/>
+        default:
+            return <div/>
+    }
+}
 
 const SortableList = SortableContainer(({items}) => {
     return (
